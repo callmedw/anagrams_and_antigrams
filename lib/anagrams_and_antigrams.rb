60eb058c9c9533e10b500d0011ff44e0db6c15aa
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-04-21 10:37:37 -0700

    add first spec to test

---
+++ lib/anagrams_and_antigrams.rb
@@ -0,0 +1,8 @@
+class String
+  define_method(:word_test) do |input_word_2|
+    result = ''
+    if self.include?(input_word_2).
+      # result =  self + input_word_2
+    end
+  end
+end
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -1,2 +1,8 @@
 require('rspec')
 require('anagrams_and_antigrams.rb')
+
+describe('String#word_test') do
+  it ('takes user input of two words and makes sure they are words') do
+    expect('spring').word_test('equinox').to(eq('these are words'))
+  end
+end
