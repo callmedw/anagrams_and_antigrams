f5d51a86f0453e0c5e84c86187a87d31db7f2746
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-04-21 15:07:54 -0700

    refactor is_word

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,14 +1,13 @@
 class String
   define_method(:is_word) do
     vowels = ["a", "e", "i", "o", "u"]
-    downcase_word = self.downcase()
-    split_word = downcase_word.split("")
+    split_word = self.downcase().split("")
     result = false
     split_word.each() do |letter|
       if vowels.include?(letter)
         result = true
       end
     end
-     result
+    result
   end
 end
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -1,9 +1,8 @@
 require('rspec')
 require('anagrams_and_antigrams.rb')
 
-
 describe ('String#is_word') do
   it("Takes an input and determines if it is word") do
-      expect('word'.is_word()).to(eq(true))
-    end
+    expect('lll'.is_word()).to(eq(false))
+  end
 end
