d986d612fc60a18f09f6429a772b5a62ae947d9c
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-04-21 15:55:00 -0700

    pass test for is_anagram test

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -11,3 +11,19 @@ class String
     result
   end
 end
+
+class String
+  define_method(:is_anagram) do |input_2|
+    first_word = self.downcase().split("")
+    second_word = input_2.downcase.split("")
+    if self.is_word() == true && input_2.is_word() == true
+      if (first_word.sort == second_word.sort)
+      'these words are anagrams.'
+      else
+      'these words are not anagrams.'
+      end
+    else
+      'these are not real words. try again using real words'
+    end
+  end
+end
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -6,3 +6,9 @@ describe ('String#is_word') do
     expect('lll'.is_word()).to(eq(false))
   end
 end
+
+describe ('String#is_anagram') do
+  it("Takes an input and determines if it is an anagram") do
+    expect('admirer'.is_anagram('married')).to(eq('these words are anagrams.'))
+  end
+end
