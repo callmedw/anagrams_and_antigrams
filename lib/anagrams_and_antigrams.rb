8865a56fc1bf6519852c350812eb6055e730008a
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-04-21 14:36:50 -0700

    update is_word spec to reflect boolean

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -3,9 +3,12 @@ class String
     vowels = ["a", "e", "i", "o", "u"]
     downcase_word = self.downcase()
     split_word = downcase_word.split("")
+    result = false
     split_word.each() do |letter|
       if vowels.include?(letter)
+        result = true
       end
     end
+     result
   end
 end
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -3,8 +3,7 @@ require('anagrams_and_antigrams.rb')
 
 
 describe ('String#is_word') do
-
   it("Takes an input and determines if it is word") do
-      expect('word'.is_word()).to(eq('word'.split("")))
+      expect('word'.is_word()).to(eq(true))
     end
 end
