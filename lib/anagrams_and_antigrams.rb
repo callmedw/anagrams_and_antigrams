9b439c9d82aaa740fb4b26acee115a9e635bb73e
Dana Weiss <mynameisdanaweiss@gmail.com>
2017-04-30 19:56:58 -0700

    add method and passing test for pass_the_test method which combines is word, is anagram, is palindrome, and is antigram methods

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -1,5 +1,6 @@
 class String
-  define_method(:is_word) do
+
+  define_method(:is_word?) do
     vowels = ["a", "e", "i", "o", "u"]
     split_word = self.downcase().split("")
     result = false
@@ -10,36 +11,26 @@ class String
     end
     result
   end
-end
 
-class String
-  define_method(:is_anagram) do |input_2|
+  define_method(:is_anagram?) do |input_2|
+    result = false
     first_word = self.downcase().split("")
     second_word = input_2.downcase.split("")
-    if self.is_word() == true && input_2.is_word() == true
       if (first_word.sort == second_word.sort)
-      'these words are anagrams.'
-      else
-      'these words are not anagrams.'
+        result = true
       end
-    else
-      'these are not real words. try again using real words'
+      result
     end
-  end
-end
 
-class String
-  define_method(:is_palindrome) do |input_2|
-    if (self.reverse == self) && (input_2.reverse == input_2)
-      'these words are palindromes.'
-    else
-      'these words are not palindromes.'
+  define_method(:is_palindrome?) do
+    result = false
+    if self == self.reverse()
+      result = true
     end
+    result
   end
-end
 
-class String
-  define_method(:is_antigram) do |input_2|
+  define_method(:is_antigram?) do |input_2|
     result = false
     first_word = self.downcase().split("")
     second_word = input_2.downcase.split("")
@@ -47,6 +38,30 @@ class String
       if second_word.include?(letter)
         result = true
       end
+    end
+    result
+  end
+
+  define_method(:pass_the_test) do |input_2|
+    result = 'blerg!'
+    first_string = self.gsub!(/[^0-9A-Za-z]/, '')
+    second_string = input_2.gsub!(/[^0-9A-Za-z]/, '')
+    if first_string.is_word?() == true && second_string.is_word?() == true
+      if first_string.is_anagram?(second_string) == true
+        if first_string.is_palindrome? == true && second_string.is_palindrome? == true
+          result = "These words are anagrams AND palindromes!"
+        else
+          result = "These are anagrams, but they are not palindromes."
+        end
+      else
+        if first_string.is_antigram?(second_string) == true
+          result = "These are not anagrams, but they are ANTIgrams!"
+        else
+          result = "These appear to be words, but they are not anagrams or antigrams."
+        end
+      end
+    else
+      result = "These are not words. Try again using real, English words."
     end
     result
   end
--- spec/anagrams_and_antigrams_spec.rb
+++ spec/anagrams_and_antigrams_spec.rb
@@ -1,26 +1,34 @@
 require('rspec')
 require('anagrams_and_antigrams.rb')
 
-describe ('String#is_word') do
-  it("Takes an input and determines if it is word") do
-    expect('lll'.is_word()).to(eq(false))
+describe('String') do
+  describe ('#is_word?') do
+    it("Takes an input and determines if it is word") do
+      expect('lll'.is_word?()).to(eq(false))
+    end
   end
-end
 
-describe ('String#is_anagram') do
-  it("Takes an input and determines if it is an anagram") do
-    expect('admirer'.is_anagram('married')).to(eq('these words are anagrams.'))
+  describe ('#is_anagram?') do
+    it("Takes an input and determines if it is an anagram") do
+      expect('admirer'.is_anagram?('married')).to(eq(true))
+    end
   end
-end
 
-describe ('String#is_palindrome') do
-  it("Takes an input and determines if it is a palindrome") do
-    expect('dad'.is_palindrome('roger')).to(eq('these words are not palindromes.'))
+  describe ('#is_palindrome?') do
+    it("Takes an input and determines if it is a palindrome") do
+      expect('dad'.is_palindrome?()).to(eq(true))
+    end
   end
-end
 
-describe ('String#is_antigram') do
-  it("Takes an input and determines if it is a antigram") do
-    expect('dad'.is_antigram('roger')).to(eq(false))
+  describe ('#is_antigram?') do
+    it("Takes an input and determines if it is a antigram") do
+      expect('dad'.is_antigram?('roger')).to(eq(false))
+    end
+  end
+
+  describe ('#pass_the_test') do
+    it("Takes two inputs of any length to determine four factors: is it a word, is it an anagram, is it a palindrome, and is it an anitgram") do
+      expect('Web Developer'.pass_the_test('Rebel Deep Vow')).to(eq("These are anagrams, but they are not palindromes."))
+    end
   end
 end
